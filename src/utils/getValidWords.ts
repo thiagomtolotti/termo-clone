b9cc95d3699c57c26fcb0a3048369fb42ca5122d
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-01-06 14:56:49 -0300

    refinamento na lista de palavras

--- src/utils/getValidWords.ts
+++ src/utils/getValidWords.ts
@@ -1,16 +1,16 @@
 import palavras from "../assets/palavras.txt";
 
-const INVALID_CHARS = ["-", ".", "/"];
+// const INVALID_CHARS = ["-", ".", "/"];
 
-function hasInvalidChar(word: string): boolean {
-  for (let i = 0; i < INVALID_CHARS.length; i++) {
-    if (word.includes(INVALID_CHARS[i])) {
-      return true;
-    }
-  }
+// function hasInvalidChar(word: string): boolean {
+//   for (let i = 0; i < INVALID_CHARS.length; i++) {
+//     if (word.includes(INVALID_CHARS[i])) {
+//       return true;
+//     }
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
 export default async function getValidWords() {
   const response = await fetch(palavras);
@@ -20,16 +20,16 @@ export default async function getValidWords() {
   }
 
   const allWordsArr = (await response.text()).split("\n");
-  const wordsWithFiveLetters: string[] = [];
+  // const wordsWithFiveLetters: string[] = [];
 
-  allWordsArr.map((word) => {
-    if (word.length === 5 && !hasInvalidChar(word)) {
-      const normalizedWord = word.normalize("NFD");
-      const wordWithoutAccent = normalizedWord.replace(/[\u0300-\u036f]/g, "");
+  // allWordsArr.map((word) => {
+  //   if (word.length === 5 && !hasInvalidChar(word)) {
+  //     const normalizedWord = word.normalize("NFD");
+  //     const wordWithoutAccent = normalizedWord.replace(/[\u0300-\u036f]/g, "");
 
-      wordsWithFiveLetters.push(wordWithoutAccent.toLowerCase());
-    }
-  });
+  //     wordsWithFiveLetters.push(wordWithoutAccent.toLowerCase());
+  //   }
+  // });
 
-  return wordsWithFiveLetters;
+  return allWordsArr;
 }
