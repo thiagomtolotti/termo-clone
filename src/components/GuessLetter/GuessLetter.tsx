ce36aacfa6e32f72b6288123bfa10bff7b883a60
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-02-16 00:25:25 -0300

    refactor GuessLetter

--- src/components/GuessLetter/GuessLetter.tsx
+++ src/components/GuessLetter/GuessLetter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./GuessLetter.module.css";
 
 interface GuessLetterProps {
@@ -12,19 +12,29 @@ const letterRegex = /^[a-zA-Z]$/;
 export const GuessLetter = ({ active = false }: GuessLetterProps) => {
   const [value, setValue] = useState("");
 
-  document.addEventListener("keydown", (ev) => {
+  const handleKeyDown = (ev: KeyboardEvent) => {
     if (!active) return;
 
     const keyPressed = ev.key;
 
     if (keyPressed === "Backspace") {
       setValue("");
+
+      return;
     }
 
     if (!letterRegex.test(keyPressed)) return;
 
     setValue(keyPressed.toUpperCase());
-  });
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <div role="guess" className={styles.guess}>
