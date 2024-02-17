b66181ccf5bd4c3c5594e493fec3fb78547d3afa
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-02-17 11:43:29 -0300

    add moves to the next GuessLetter when a letter is inputed

--- src/components/GuessRow/GuessRow.test.tsx
+++ src/components/GuessRow/GuessRow.test.tsx
@@ -1,107 +1,123 @@
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { GuessRow } from "./GuessRow";
 
 describe("GuessRow element", () => {
-	it("Should render correctly", () => {
-		const { container } = render(<GuessRow />);
+  it("Should render correctly", () => {
+    const { container } = render(<GuessRow />);
 
-		expect(container).toMatchSnapshot();
-	});
+    expect(container).toMatchSnapshot();
+  });
 
-	describe("if is active", () => {
-		it("Should have at least one active guess", () => {
-			const { getAllByRole } = render(<GuessRow active />);
+  describe("if is active", () => {
+    it("Should have at least one active guess", () => {
+      const { getAllByRole } = render(<GuessRow active />);
 
-			const guessLetters = getAllByRole("guess");
+      const guessLetters = getAllByRole("guess");
 
-			const hasActiveGuess = guessLetters.some((guesLetter) =>
-				guesLetter.classList.contains("active")
-			);
+      const hasActiveGuess = guessLetters.some((guesLetter) =>
+        guesLetter.classList.contains("active")
+      );
 
-			expect(hasActiveGuess).toBe(true);
-		});
+      expect(hasActiveGuess).toBe(true);
+    });
 
-		it("Should allow the user to click on the guesses to make it active", () => {
-			const { getAllByRole } = render(<GuessRow active />);
+    it("Should allow the user to click on the guesses to make it active", () => {
+      const { getAllByRole } = render(<GuessRow active />);
 
-			const guessLetters = getAllByRole("guess");
+      const guessLetters = getAllByRole("guess");
 
-			fireEvent.click(guessLetters[2]);
+      fireEvent.click(guessLetters[2]);
 
-			expect(guessLetters[2].classList.contains("active")).toBe(true);
-			expect(guessLetters[0].classList.contains("active")).toBe(false);
-		});
-		describe("Arrow clicks", () => {
-			const arrowRightKeyDownEvt = {
-				key: "ArrowRight",
-				code: "ArrowRight",
-			};
-			const arrowLeftKeyDownEvt = {
-				key: "ArrowLeft",
-				code: "ArrowLeft",
-			};
+      expect(guessLetters[2].classList.contains("active")).toBe(true);
+      expect(guessLetters[0].classList.contains("active")).toBe(false);
+    });
+    describe("Arrow clicks", () => {
+      const arrowRightKeyDownEvt = {
+        key: "ArrowRight",
+        code: "ArrowRight",
+      };
+      const arrowLeftKeyDownEvt = {
+        key: "ArrowLeft",
+        code: "ArrowLeft",
+      };
 
-			it("Should allow to toggle the active guess by clicking the arrows", () => {
-				const { getAllByRole } = render(<GuessRow active />);
-				const guessLetters = getAllByRole("guess");
+      it("Should allow to toggle the active guess by clicking the arrows", () => {
+        const { getAllByRole } = render(<GuessRow active />);
+        const guessLetters = getAllByRole("guess");
 
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
 
-				expect(guessLetters[1].classList.contains("active")).toBe(true);
-				expect(guessLetters[0].classList.contains("active")).toBe(
-					false
-				);
+        expect(guessLetters[1].classList.contains("active")).toBe(true);
+        expect(guessLetters[0].classList.contains("active")).toBe(false);
 
-				fireEvent.keyDown(document.body, arrowLeftKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowLeftKeyDownEvt);
 
-				expect(guessLetters[0].classList.contains("active")).toBe(true);
-			});
+        expect(guessLetters[0].classList.contains("active")).toBe(true);
+      });
 
-			it("Shouldn't allow to go below 0", () => {
-				const { getAllByRole } = render(<GuessRow active />);
-				const guessLetters = getAllByRole("guess");
+      it("Shouldn't allow to go below 0", () => {
+        const { getAllByRole } = render(<GuessRow active />);
+        const guessLetters = getAllByRole("guess");
 
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
 
-				expect(guessLetters[1].classList.contains("active")).toBe(true);
+        expect(guessLetters[1].classList.contains("active")).toBe(true);
 
-				fireEvent.keyDown(document.body, arrowLeftKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowLeftKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowLeftKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowLeftKeyDownEvt);
 
-				expect(guessLetters[0].classList.contains("active")).toBe(true);
-			});
+        expect(guessLetters[0].classList.contains("active")).toBe(true);
+      });
 
-			it("Shouldn't allow to go after max size", () => {
-				const { getAllByRole } = render(<GuessRow active />);
+      it("Shouldn't allow to go after max size", () => {
+        const { getAllByRole } = render(<GuessRow active />);
 
-				const guessLetters = getAllByRole("guess");
+        const guessLetters = getAllByRole("guess");
 
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
-				fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
+        fireEvent.keyDown(document.body, arrowRightKeyDownEvt);
 
-				expect(guessLetters[4].classList.contains("active")).toBe(true);
-			});
-		});
-	});
+        expect(guessLetters[4].classList.contains("active")).toBe(true);
+      });
+    });
 
-	describe("it is not active", () => {
-		it("Shouldn't have an active guess", () => {
-			const { getAllByRole } = render(<GuessRow />);
+    // TODO: It should move to the next EMPTY GuessLetter
+    it("Should go to the next GuessLetter when a value is assigned", () => {
+      const keyDownEvent = {
+        key: "T",
+        code: "T",
+      };
 
-			const guessLetters = getAllByRole("guess-wrapper");
+      render(<GuessRow active />);
+      const guessLetters = screen.getAllByRole("guess");
 
-			const hasActiveGuess = guessLetters.some((guesLetter) =>
-				guesLetter.classList.contains("active")
-			);
+      expect(guessLetters[0].classList.contains("active")).toBe(true);
 
-			expect(hasActiveGuess).toBe(false);
-		});
-	});
+      fireEvent.keyDown(document.body, keyDownEvent);
+
+      expect(guessLetters[1].classList.contains("active")).toBe(true);
+      expect(guessLetters[0].classList.contains("active")).toBe(false);
+    });
+  });
+
+  describe("it is not active", () => {
+    it("Shouldn't have an active guess", () => {
+      const { getAllByRole } = render(<GuessRow />);
+
+      const guessLetters = getAllByRole("guess-wrapper");
+
+      const hasActiveGuess = guessLetters.some((guesLetter) =>
+        guesLetter.classList.contains("active")
+      );
+
+      expect(hasActiveGuess).toBe(false);
+    });
+  });
 });
--- src/components/GuessRow/GuessRow.tsx
+++ src/components/GuessRow/GuessRow.tsx
@@ -5,43 +5,53 @@ import { GuessLetter } from "../GuessLetter/GuessLetter";
 import styles from "./GuessRow.module.css";
 
 const WORD_SIZE = 5;
+const letterRegex = /^[a-zA-Z]$/;
 
 interface GuessRowProps {
-	active?: boolean;
+  active?: boolean;
 }
 
 export const GuessRow = ({ active = false }: GuessRowProps) => {
-	const [activeGuessIndex, setActiveGuessIndex] = useState(0);
+  const [activeGuessIndex, setActiveGuessIndex] = useState(0);
 
-	useEffect(() => {
-		const handleArrowClick = (ev: KeyboardEvent) => {
-			if (ev.key === "ArrowRight") {
-				setActiveGuessIndex((activeIndex) =>
-					Math.min(activeIndex + 1, WORD_SIZE - 1)
-				);
-			} else if (ev.key === "ArrowLeft") {
-				setActiveGuessIndex((activeIndex) =>
-					activeIndex !== null ? Math.max(activeIndex - 1, 0) : 0
-				);
-			}
-		};
+  useEffect(() => {
+    const handleArrowClick = (ev: KeyboardEvent) => {
+      if (ev.key === "ArrowRight") {
+        setActiveGuessIndex((activeIndex) =>
+          Math.min(activeIndex + 1, WORD_SIZE - 1)
+        );
+      } else if (ev.key === "ArrowLeft") {
+        setActiveGuessIndex((activeIndex) =>
+          activeIndex !== null ? Math.max(activeIndex - 1, 0) : 0
+        );
+      }
+    };
+    const moveActiveLetterWhenLetterInput = (ev: KeyboardEvent) => {
+      if (!letterRegex.test(ev.key)) return;
 
-		document.addEventListener("keydown", handleArrowClick);
+      setActiveGuessIndex((activeGuessIndex) =>
+        Math.min(activeGuessIndex + 1, WORD_SIZE - 1)
+      );
+    };
 
-		return () => {
-			document.removeEventListener("keydown", handleArrowClick);
-		};
-	}, [activeGuessIndex]);
+    document.addEventListener("keydown", handleArrowClick);
+    document.addEventListener("keydown", moveActiveLetterWhenLetterInput);
 
-	return [...Array(WORD_SIZE)].map((_, index) => (
-		<div
-			onClick={() => {
-				setActiveGuessIndex(index);
-			}}
-			role="guess-wrapper"
-			key={index}
-		>
-			<GuessLetter active={active && activeGuessIndex === index} />
-		</div>
-	));
+    return () => {
+      document.removeEventListener("keydown", handleArrowClick);
+      document.removeEventListener("keydown", moveActiveLetterWhenLetterInput);
+    };
+  }, [activeGuessIndex]);
+
+  return [...Array(WORD_SIZE)].map((_, index) => (
+    <div
+      onClick={() => {
+        setActiveGuessIndex(index);
+      }}
+      role="guess-wrapper"
+      key={index}
+    >
+      <GuessLetter active={active && activeGuessIndex === index} />
+    </div>
+  ));
 };
