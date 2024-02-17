0aaf8b62284f5cc57695c99ef76713f3f11ef0ea
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-02-17 11:19:34 -0300

    add tests for changing prop active on GuessLetter component

--- src/components/GuessLetter/GuessLetter.test.tsx
+++ src/components/GuessLetter/GuessLetter.test.tsx
@@ -2,98 +2,131 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { GuessLetter } from "./GuessLetter";
 
 describe("GuessLetter component", () => {
-	const keyDownEvent = {
-		key: "T",
-		code: "T",
-	};
-	let guess: HTMLElement;
+  const keyDownEvent = {
+    key: "T",
+    code: "T",
+  };
+  let guess: HTMLElement;
 
-	it("Should render correctly", () => {
-		const { container } = render(<GuessLetter />);
+  it("Should render correctly", () => {
+    const { container } = render(<GuessLetter />);
 
-		expect(container).toMatchSnapshot();
-	});
+    expect(container).toMatchSnapshot();
+  });
 
-	describe("if is active", () => {
-		beforeEach(() => {
-			render(<GuessLetter active />);
-			guess = screen.getByRole("guess");
-		});
+  describe("if is active", () => {
+    beforeEach(() => {
+      render(<GuessLetter active />);
+      guess = screen.getByRole("guess");
+    });
 
-		it("If receives an input it should have it's inner value set to the input value", () => {
-			fireEvent.keyDown(document.body, keyDownEvent);
+    it("If receives an input it should have it's inner value set to the input value", () => {
+      fireEvent.keyDown(document.body, keyDownEvent);
 
-			expect(guess.innerHTML).toBe(keyDownEvent.key);
-		});
+      expect(guess.innerHTML).toBe(keyDownEvent.key);
+    });
 
-		it("If receives a new value it should substitute it's inner value", () => {
-			fireEvent.keyDown(document.body, keyDownEvent);
+    it("If receives a new value it should substitute it's inner value", () => {
+      fireEvent.keyDown(document.body, keyDownEvent);
 
-			expect(guess.innerHTML).toBe(keyDownEvent.key);
+      expect(guess.innerHTML).toBe(keyDownEvent.key);
 
-			const newKeyDownEvent = {
-				key: "Z",
-				code: "Z",
-			};
-			fireEvent.keyDown(document.body, newKeyDownEvent);
+      const newKeyDownEvent = {
+        key: "Z",
+        code: "Z",
+      };
+      fireEvent.keyDown(document.body, newKeyDownEvent);
 
-			expect(guess.innerHTML).toBe(newKeyDownEvent.key);
-		});
+      expect(guess.innerHTML).toBe(newKeyDownEvent.key);
+    });
 
-		it("Should convert the value of the key pressed to uppercase", () => {
-			const smallCaseKeyDownEvt = {
-				key: "t",
-				code: "t",
-			};
+    it("Should convert the value of the key pressed to uppercase", () => {
+      const smallCaseKeyDownEvt = {
+        key: "t",
+        code: "t",
+      };
 
-			fireEvent.keyDown(document.body, smallCaseKeyDownEvt);
+      fireEvent.keyDown(document.body, smallCaseKeyDownEvt);
 
-			expect(guess.innerHTML).toBe(smallCaseKeyDownEvt.key.toUpperCase());
-		});
+      expect(guess.innerHTML).toBe(smallCaseKeyDownEvt.key.toUpperCase());
+    });
 
-		it("Should ignore inputs that are not letters", () => {
-			fireEvent.keyDown(document.body, keyDownEvent);
+    it("Should ignore inputs that are not letters", () => {
+      fireEvent.keyDown(document.body, keyDownEvent);
 
-			const AltKeyDownEvt = {
-				key: "Alt",
-				code: "Alt",
-			};
+      const AltKeyDownEvt = {
+        key: "Alt",
+        code: "Alt",
+      };
 
-			fireEvent.keyDown(document.body, AltKeyDownEvt);
+      fireEvent.keyDown(document.body, AltKeyDownEvt);
 
-			expect(guess.innerHTML).toBe(keyDownEvent.code);
-		});
+      expect(guess.innerHTML).toBe(keyDownEvent.code);
+    });
 
-		it("Should be empty when backspace is pressed", () => {
-			const backspaceKeyDownEvt = {
-				key: "Backspace",
-				code: "Backspace",
-			};
+    it("Should be empty when backspace is pressed", () => {
+      const backspaceKeyDownEvt = {
+        key: "Backspace",
+        code: "Backspace",
+      };
 
-			fireEvent.keyDown(document.body, keyDownEvent);
-			fireEvent.keyDown(document.body, backspaceKeyDownEvt);
+      fireEvent.keyDown(document.body, keyDownEvent);
+      fireEvent.keyDown(document.body, backspaceKeyDownEvt);
 
-			expect(guess.innerHTML).toBe("");
-		});
-	});
+      expect(guess.innerHTML).toBe("");
+    });
+  });
 
-	it("If is not active and receives an input it should do nothing", () => {
-		render(<GuessLetter />);
+  it("If is not active and receives an input it should do nothing", () => {
+    render(<GuessLetter />);
 
-		const guess = screen.getByRole("guess");
+    const guess = screen.getByRole("guess");
 
-		fireEvent.keyDown(document.body, keyDownEvent);
+    fireEvent.keyDown(document.body, keyDownEvent);
 
-		expect(guess.innerHTML).toBe("");
-	});
+    expect(guess.innerHTML).toBe("");
+  });
 
-	describe("When active is toggled", () => {
-		it("Should disable the keyDown events when it isn't active", () => {
-			expect(true).toBeFalsy();
-		});
+  describe("When active is toggled", () => {
+    it("Should disable the keyDown events when it isn't active", () => {
+      const { rerender } = render(<GuessLetter active />);
 
-		it("Should enable the keyDown events when it is active", () => {
-			expect(true).toBeFalsy();
-		});
-	});
+      const guess = screen.getByRole("guess");
+      fireEvent.keyDown(document.body, keyDownEvent);
+
+      expect(guess.innerHTML).toBe(keyDownEvent.key);
+
+      rerender(<GuessLetter />);
+
+      const newKeyDownEvent = {
+        key: "Z",
+        code: "Z",
+      };
+
+      fireEvent.keyDown(document.body, newKeyDownEvent);
+
+      expect(guess.innerHTML).toBe(keyDownEvent.key);
+    });
+
+    it("Should enable the keyDown events when it is active", () => {
+      const { rerender } = render(<GuessLetter />);
+
+      const guess = screen.getByRole("guess");
+
+      fireEvent.keyDown(document.body, keyDownEvent);
+
+      expect(guess.innerHTML).toBe("");
+
+      rerender(<GuessLetter active />);
+
+      const newKeyDownEvent = {
+        key: "Z",
+        code: "Z",
+      };
+
+      fireEvent.keyDown(document.body, newKeyDownEvent);
+
+      expect(guess.innerHTML).toBe(newKeyDownEvent.key);
+    });
+  });
 });
