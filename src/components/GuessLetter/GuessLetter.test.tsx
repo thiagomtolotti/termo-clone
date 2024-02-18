999822159c97b799c06e8d1aa52c4692e3fdaf33
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-02-18 12:14:59 -0300

    refactor tests for GuessLetter

--- src/components/GuessLetter/GuessLetter.test.tsx
+++ src/components/GuessLetter/GuessLetter.test.tsx
@@ -130,3 +130,20 @@ import { GuessLetter } from "./GuessLetter";
 //     });
 //   });
 // });
+
+describe("GuessLetter component", () => {
+  it("Should render correctly", () => {
+    const { container } = render(<GuessLetter>T</GuessLetter>);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("Should render correctly if it's active", () => {
+    const { container } = render(<GuessLetter active>T</GuessLetter>);
+
+    expect(container).toMatchSnapshot();
+
+    const guessElement = screen.getByRole("guess");
+    expect(guessElement.classList.contains("active")).toBe(true);
+  });
+});
