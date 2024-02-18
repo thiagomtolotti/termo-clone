1dd7d4086625b7cd032aeb71ff3377f9a4637132
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-02-18 20:32:41 -0300

    logic on guessArea component

--- src/components/GuessArea/GuessArea.tsx
+++ src/components/GuessArea/GuessArea.tsx
@@ -1,28 +1,104 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { GuessRow } from "../GuessRow/GuessRow";
 
 const NUMBER_OF_GUESSES = 6;
+const WORD_SIZE = 5;
 
 export const GuessArea = () => {
   const [activeRow, setActiveRow] = useState(0);
+  const [activeGuessIndex, setActiveGuessIndex] = useState(0);
+  const [rowsValue, setRowsValue] = useState(
+    Array.from({ length: NUMBER_OF_GUESSES }, () => Array(WORD_SIZE).fill(""))
+  );
+
+  const letterRegex = /^[a-zA-Z]$/;
 
   useEffect(() => {
+    const handleLetterClick = (ev: KeyboardEvent) => {
+      const keyPressed = ev.key;
+
+      if (!letterRegex.test(keyPressed)) return;
+
+      setRowsValue((rowsValue) => {
+        const newValue = [...rowsValue];
+        const activeRowValue = newValue[activeRow];
+
+        activeRowValue[activeGuessIndex] = keyPressed.toUpperCase();
+
+        return newValue;
+      });
+
+      setActiveGuessIndex((activeGuessIndex) =>
+        Math.min(activeGuessIndex + 1, WORD_SIZE - 1)
+      );
+    };
+
+    const handleBackspaceClick = (ev: KeyboardEvent) => {
+      const isBackspace = ev.key === "Backspace";
+
+      if (!isBackspace) return;
+
+      setRowsValue((rowValue) => {
+        const newValue = [...rowsValue];
+        const activeRowValue = newValue[activeRow];
+
+        activeRowValue[activeGuessIndex] = "";
+
+        return newValue;
+      });
+
+      setActiveGuessIndex((activeIndex) => Math.max(activeIndex - 1, 0));
+    };
+
+    const handleArrowClick = (ev: KeyboardEvent) => {
+      const isArrowKey = ev.key === "ArrowLeft" || ev.key === "ArrowRight";
+
+      if (!isArrowKey) return;
+
+      if (ev.key === "ArrowRight") {
+        setActiveGuessIndex((activeIndex) =>
+          Math.min(activeIndex + 1, WORD_SIZE - 1)
+        );
+
+        return;
+      }
+
+      setActiveGuessIndex((activeIndex) => Math.max(activeIndex - 1, 0));
+    };
+
     const handleEnterClick = (ev: KeyboardEvent) => {
       if (!ev.key.toLowerCase().includes("enter")) return;
+
+      if (rowsValue[activeRow].indexOf("") !== -1) return;
 
       setActiveRow((activeRow) => Math.min(activeRow + 1, NUMBER_OF_GUESSES));
     };
 
+    document.addEventListener("keydown", handleLetterClick);
+    document.addEventListener("keydown", handleBackspaceClick);
+    document.addEventListener("keydown", handleArrowClick);
     document.addEventListener("keydown", handleEnterClick);
 
     return () => {
+      document.removeEventListener("keydown", handleLetterClick);
+      document.removeEventListener("keydown", handleBackspaceClick);
+      document.removeEventListener("keydown", handleArrowClick);
       document.removeEventListener("keydown", handleEnterClick);
     };
+  }, [activeGuessIndex]);
+
+  useLayoutEffect(() => {
+    setActiveGuessIndex(0);
   }, [activeRow]);
 
-  return [...Array(NUMBER_OF_GUESSES)].map((_, index) => (
-    <GuessRow active={index === activeRow} />
+  return rowsValue.map((rowValue, index) => (
+    <GuessRow
+      active={activeRow === index}
+      value={rowValue}
+      key={index}
+      activeIndex={activeGuessIndex}
+    />
   ));
 };
--- src/components/GuessRow/GuessRow.tsx
+++ src/components/GuessRow/GuessRow.tsx
@@ -1,99 +1,26 @@
-"use client";
-
-import { useCallback, useEffect, useLayoutEffect, useState } from "react";
 import { GuessLetter } from "../GuessLetter/GuessLetter";
 import styles from "./GuessRow.module.css";
 
 interface GuessRowProps {
   active?: boolean;
+  value: string[];
+  activeIndex: number;
 }
 
-export const GuessRow = ({ active = false }: GuessRowProps) => {
-  const WORD_SIZE = 5;
-  const letterRegex = /^[a-zA-Z]$/;
-
-  const [activeGuessIndex, setActiveGuessIndex] = useState(0);
-  const [rowValue, setRowValue] = useState(Array(WORD_SIZE).fill(""));
-
-  const handleBackspaceClick = (ev: KeyboardEvent) => {
-    const isBackspace = ev.key === "Backspace";
-
-    if (!isBackspace) return;
-
-    setRowValue((value) => {
-      const newValue = [...value];
-
-      newValue[activeGuessIndex] = "";
-
-      return newValue;
-    });
-
-    setActiveGuessIndex((activeIndex) => Math.max(activeIndex - 1, 0));
-  };
-
-  const handleArrowClick = (ev: KeyboardEvent) => {
-    const isArrowKey = ev.key === "ArrowLeft" || ev.key === "ArrowRight";
-
-    if (!isArrowKey) return;
-
-    if (ev.key === "ArrowRight") {
-      setActiveGuessIndex((activeIndex) =>
-        Math.min(activeIndex + 1, WORD_SIZE - 1)
-      );
-
-      return;
-    }
-
-    setActiveGuessIndex((activeIndex) => Math.max(activeIndex - 1, 0));
-  };
-
-  useLayoutEffect(() => {
-    setActiveGuessIndex(0);
-  }, [active]);
-
-  useEffect(() => {
-    const handleLetterClick = (ev: KeyboardEvent) => {
-      const keyPressed = ev.key;
-
-      if (!letterRegex.test(keyPressed)) return;
-
-      setRowValue((value) => {
-        const newValue = [...value];
-
-        newValue[activeGuessIndex] = keyPressed.toUpperCase();
-
-        return newValue;
-      });
-
-      setActiveGuessIndex((activeGuessIndex) =>
-        Math.min(activeGuessIndex + 1, WORD_SIZE - 1)
-      );
-    };
-
-    if (active) {
-      document.addEventListener("keydown", handleLetterClick);
-      document.addEventListener("keydown", handleBackspaceClick);
-      document.addEventListener("keydown", handleArrowClick);
-    }
-
-    return () => {
-      document.removeEventListener("keydown", handleLetterClick);
-      document.removeEventListener("keydown", handleBackspaceClick);
-      document.removeEventListener("keydown", handleArrowClick);
-    };
-  }, [active, activeGuessIndex]);
-
+export const GuessRow = ({
+  active = false,
+  activeIndex,
+  value,
+}: GuessRowProps) => {
   return (
     <div
       className={`${styles.guessRow} ${active ? styles.active : ""}`}
       role="guess-row"
     >
-      {rowValue.map((value, index) => (
-        <div onClick={() => setActiveGuessIndex(index)} key={index}>
-          <GuessLetter active={active && activeGuessIndex === index}>
-            {value}
-          </GuessLetter>
-        </div>
+      {value.map((value, index) => (
+        <GuessLetter active={active && activeIndex === index}>
+          {value}
+        </GuessLetter>
       ))}
     </div>
   );
