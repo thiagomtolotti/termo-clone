56bf1fbe8e4ef820dd93b0abdbbd7bd5d7971817
Thiago Tolotti <thiagotolotti@gmail.com>
2025-01-19 20:23:09 -0300

    GuessLetter with border-box

--- src/components/GuessArea/GuessRow/GuessLetter/GuessLetter.tsx
+++ src/components/GuessArea/GuessRow/GuessLetter/GuessLetter.tsx
@@ -19,11 +19,11 @@ export const GuessLetter = ({
     <div
       role="guess"
       className={clsx(
-        "w-full flex items-center justify-center font-bold aspect-square rounded-md text-4xl",
+        "w-full flex items-center justify-center font-bold aspect-square rounded-md text-4xl box-border",
         isRowActive &&
           "bg-transparent border-4 border-brown-300 cursor-pointer",
         !isRowActive && Boolean(children) ? "bg-brown-100" : "bg-brown-400",
-        active && "!border-b-[10px]",
+        active && "!border-b-8",
         isCorrectOrMisplaced === "correct" && "bg-green",
         isCorrectOrMisplaced === "misplaced" && "bg-yellow"
       )}
